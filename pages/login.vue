b88f4f17a995364cb970573fa46cf1cd7d03a744
Nadun Dileepa <nadun.virtual@gmail.com>
2021-04-12 23:20:59 +0530

    add login ui and login funtion

---
+++ pages/login.vue
@@ -0,0 +1,90 @@
+<template>
+	<div class="login-container">
+		<div class="form-container">
+			<label for="email">Username:</label><br>
+			<input type="email" v-model="username" id="email" name="email" class="login-input"><br>
+			<label for="pwd">Password:</label><br>
+			<input type="password"  v-model="password" id="pwd" name="pwd" class="login-input"><br><br>
+			<input id="submit-btn" type="submit" @click="login">
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	data: function () {
+		return {
+			username:"",
+			password: "",
+			mountains:{}
+		}
+	},
+	methods:{
+		login: async function() {
+
+			let obj = {
+				"email":this.username, // eve.holt@reqres.in
+				"password":this.password //cityslicka
+			};
+
+			var myHeaders = new Headers();
+			myHeaders.append("Content-Type", "application/json");
+			myHeaders.append("Cookie", "__cfduid=ddfe91fe29ce404c0606b4094e0e912bd1618244858");
+
+			var raw = JSON.stringify(obj);
+
+			var requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+			};
+
+			fetch("https://reqres.in/api/login", requestOptions)
+			.then(response => response.text())
+			.then(
+				result => console.log(result)
+			).then(
+				this.$router.push('/')
+			)
+			.catch(
+				error => console.log('error', error)
+			);
+			// if (response.status === 200) {
+			// 	routes.push({
+			// 		path: "/",
+			// 		component: _09f5b4e0,
+			// 		name: "index"
+			// 	})
+				// this.$router.push('/')
+				// await nuxt.renderRoute('/')
+				// location.reload()
+				// return redirect('http://localhost:3000/')
+			// }
+		}
+	}
+}
+</script>
+
+<style scoped>
+.login-container {
+	display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+	height: 100vh;
+	background-color: #23282C;
+}
+.form-container {
+	display: flex;
+    flex-direction: column;
+	width: 40%;
+	padding: 5%;
+	border-radius: 10px;
+	background-color: #fff;
+}
+
+#submit-btn {
+	
+}
+
+</style>
