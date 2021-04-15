f32a0b45532d2622885cae154bd3a49a12d7cb7a
Jayanga Jayathilake <jayanga.j@aeturnum.com>
2021-04-15 13:37:11 +0530

    feat: added local storage persistance

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,28 @@
         <h1 class="title">
           {{ hello }}
         </h1>
-        <button v-if="!isFullScreen" @click="goFullScreen()">Go fullscreen</button>
+        <div>
+          Local Storage Used Capacity
+          {{ capacity.localStorage }}
+        </div>
+        <button v-if="!isFullScreen" @click="goFullScreen()">
+          Go fullscreen
+        </button>
         <button v-else @click="exitFullscreen()">Exit Fullscreen</button>
         <button @click="fetchData()">Fetch Data</button>
+        <br />
+        <button @click="saveToLocalStorage()">
+          Save Data to Local Storage
+        </button>
+        <button @click="appendDataToLocalStorage()">
+          Append Data to Local Storage
+        </button>
+        <br />
+        <button @click="saveToIndexedDB()">Save Data to IndexedDB</button>
+        <button @click="appendDataToIndexedDB()">
+          Append Data to IndexedDB
+        </button>
+        <br />
         <ul v-if="posts.length">
           <li v-for="post in posts" v-bind:key="post.id">{{ post.body }}</li>
         </ul>
@@ -37,8 +56,20 @@ export default {
     return {
       hello: "Vue PWA test",
       posts: [],
+      localStoragePosts: [],
+      indexedDBPosts: [],
       isFullScreen: false,
+      // capacity: {
+      //   localStorage: 0,
+      // },
     };
+  },
+  computed: {
+    capacity() {
+      return {
+        localStorage: this.localStoragePosts.length,
+      };
+    },
   },
   methods: {
     goFullScreen() {
@@ -57,7 +88,7 @@ export default {
     },
     exitFullscreen() {
       const comp = this;
-    
+
       document
         .exitFullscreen()
         .then(function () {
@@ -77,9 +108,31 @@ export default {
           this.posts = data;
         });
     },
+    saveToLocalStorage() {
+      localStorage.setItem("posts-data", JSON.stringify(this.posts));
+    },
+    appendDataToLocalStorage() {
+      const prevPosts = JSON.parse(localStorage.getItem("posts-data"));
+      // debugger;
+      console.log(`LOCAL STORAGE: Prev Posts: ${prevPosts.length}`, prevPosts);
+      const newPosts = prevPosts.concat(this.posts);
+      localStorage.setItem("posts-data", JSON.stringify(newPosts));
+      console.log(`LOCAL STORAGE: New Post List: ${newPosts.length}`, newPosts);
+    },
+    saveToIndexedDB() {
+      this.$idb.set("posts-data", this.posts);
+    },
+    appendDataToIndexedDB() {
+      const prevPosts = this.$idb.get("posts-data");
+      // this.$idb.set("posts-data", );
+    },
+    loadStoredData() {
+      this.localStoragePosts = JSON.parse(localStorage.getItem("posts-data"));
+    },
   },
   mounted() {
     // this.fetchData();
+    this.loadStoredData();
   },
 };
 </script>
