3218423c58a9054014d9ea015d5c6b90ab684419
Jayanga Jayathilake <jayanga.j@aeturnum.com>
2021-04-15 13:38:17 +0530

    feat: added Indexed DB wrapper

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/sw.client.js'
+    '~/plugins/sw.client.js',
+    '~/plugins/idb.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
