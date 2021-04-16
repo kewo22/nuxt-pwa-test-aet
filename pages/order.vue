33cb4ce4a3834b6bcacecb1ae1fd2e195357a65a
SajithL-Aet <sajith.l@aeturnum.com>
2021-04-16 12:55:28 +0530

    order ui break to sections

---
+++ pages/order.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container fluid>
+    <v-layout>
+      <v-flex xs1 sm1 md1>
+        <v-row align="center" justify="center" style="height:100vh">
+          <v-col
+            cols="12"
+            lg="6"
+            md="6"
+            class="fill-height d-flex flex-column justify-center align-center"
+          >
+            left
+          </v-col>
+        </v-row>
+      </v-flex>
+      <v-flex xs11 sm11 md11>
+        <v-layout>
+          <v-flex xs6 sm6 md6>
+            <v-row align="center" justify="center" style="height:100vh">
+              <v-col
+                cols="12"
+                lg="6"
+                md="6"
+                class="fill-height d-flex flex-column justify-center align-center"
+              >
+                order queue
+              </v-col>
+            </v-row>
+          </v-flex>
+          <v-flex xs6 sm6 md6>
+            <v-row align="center" justify="center" style="height:100vh">
+              <v-col
+                cols="12"
+                lg="6"
+                md="6"
+                class="fill-height d-flex flex-column justify-center align-center"
+              >
+                selected order
+              </v-col>
+            </v-row>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
