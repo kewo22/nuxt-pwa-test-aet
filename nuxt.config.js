d28534b7cb5ffce1b3a190a2cbf18f37e05805bf
Jayanga Jayathilake <jayanga.j@aeturnum.com>
2021-04-18 01:42:58 +0530

    fix: updated nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/sw.client.js',
     '~/plugins/idb.client.js'
   ],
 
