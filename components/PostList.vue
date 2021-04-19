390936f5a3cca2f92fad1ba41a7657790ed68a1b
Jayanga Jayathilake <jayanga.j@aeturnum.com>
2021-04-19 10:58:31 +0530

    fix: updated post list styling

--- components/PostList.vue
+++ components/PostList.vue
@@ -1,15 +1,21 @@
 <template>
-  <ul v-if="posts.length">
-    <li v-for="post in posts" v-bind:key="`${post.id}${Date.now()}`">
+  <ul v-if="posts.length" class="overflow">
+    <li v-for="post in posts" :key="`${post.id}${Date.now()}`">
       {{ post.body }}
     </li>
   </ul>
 </template>
 
+<style>
+.overflow {
+  margin-top: 20px;
+  overflow: auto;
+  max-height: 60vh;
+}
+</style>
+
 <script>
 export default {
-    props: [
-        'posts'
-    ]
-}
+  props: ["posts"],
+};
 </script>
