a32b97919b6e6d4de229e666efc2454e007d3581
Jayanga Jayathilake <jayanga.j@aeturnum.com>
2021-04-20 12:01:41 +0530

    feat: added storage quota counters to visualizations side pane

--- components/DataViz.vue
+++ components/DataViz.vue
@@ -2,18 +2,26 @@
   <div class="data-viz">
     <h2 class="title">SW Data Visualizer</h2>
     <div class="flex-1">
-      <h3>Local Storage Data (Posts count: {{ localStorageData.length }})</h3>
+      <h3>
+        Local Storage Data (Posts count: {{ localStorageData.length }})
+        <storage-quota :data="localStorageData" />
+      </h3>
       <post-list :posts="localStorageData" />
     </div>
     <div class="flex-1">
-      <h3>Indexed DB Data (Posts count: {{ indexedDbData.length }})</h3>
+      <h3>
+        Indexed DB Data (Posts count: {{ indexedDbData.length }})
+        <storage-quota :data="indexedDbData" />
+      </h3>
       <post-list :posts="indexedDbData" />
     </div>
   </div>
 </template>
 
 <script>
+import StorageQuota from "./StorageQuota.vue";
 export default {
+  components: { StorageQuota },
   data() {
     return {
       localStorageData: [],
