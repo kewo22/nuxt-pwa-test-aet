26681d75aa08afb660ad88931a5941dc5dd7fd01
Jayanga Jayathilake <jayanga.j@aeturnum.com>
2021-04-20 11:39:43 +0530

    feat: added quota calculation

--- components/StorageQuota.vue
+++ components/StorageQuota.vue
@@ -3,20 +3,41 @@
 </template>
 
 <script>
-const calculateStorageQuota = (dataSet) => {
-  console.log(`CALC:`, dataSet);
+import * as sizeof from "object-sizeof";
 
-  let size = 0;
-  let unit = "bytes";
+const ONE_MEG = 1000000;
+
+const calculateStorageQuota = (dataSet) => {
+  const byteSize = sizeof(dataSet);
+  const size = formatToMB(byteSize);
+  let unit = "";
   return { size, unit };
 };
+
+/* Format the number into something nice if possible */
+function formatToMB(val) {
+  const opts = {
+    maximumFractionDigits: 10,
+  };
+  let result;
+  try {
+    result = new Intl.NumberFormat("en-us", opts).format(val / ONE_MEG);
+  } catch (ex) {
+    result = Math.round(val / ONE_MEG);
+  }
+  return `${result} MB`;
+}
 
 export default {
   props: ["data"],
   computed: {
     quota() {
+      //   navigator.storage
+      //     .estimate()
+      //     // .then((quota) => {})
+      //     .then(console.log)
+      //     .catch(console.error);
       return calculateStorageQuota(this.$props.data);
-      return { size: 0, unit: "MB" };
     },
   },
 };
