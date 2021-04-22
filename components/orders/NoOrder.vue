375dc9881d6985fe7f1bf456f185db9299d3db4d
Jayanga Jayathilake <jayanga.j@aeturnum.com>
2021-04-22 11:00:58 +0530

    fix: css fixes no-selected-order component

--- components/orders/NoOrder.vue
+++ components/orders/NoOrder.vue
@@ -3,8 +3,10 @@
     <v-row align="center" justify="center">
       <v-col align="center" justify="center">
         <img src="/noOrder.png" /><br />
-        <h2 style="margin-top: 4%">NO ORDER SELECTED</h2>
-        <h4 style="color: #c3c5c8; margin-top: 1%">
+        <h2 class="black--text text--darken-1 mt-4">
+          NO ORDER SELECTED
+        </h2>
+        <h4 class="mt-1" style="color: #A0A2A7;">
           Select an order from the queue on the left
         </h4>
       </v-col>
