44d01e5e68fe43f23ced017f9ab18a253c1752df
KewinFernando <kewinf271@gmail.com>
2021-04-23 16:47:41 +0530

    login layout added

---
+++ layouts/empty.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app light> 
+    <v-main>
+      <nuxt />
+    </v-main>
+
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+    };
+  },
+  created() {},
+};
+</script>
