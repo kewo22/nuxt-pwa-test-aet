a8f63293eef4fb2cf9aaf96a1bb232b0c62fe2ff
Jayanga Jayathilake <jayangaaj@gmail.com>
2021-05-04 20:16:31 +0530

    fix: swipe styling updated

--- components/orders/OrderQueueItem.vue
+++ components/orders/OrderQueueItem.vue
@@ -1,78 +1,82 @@
 <template>
   <div class="swipeable-stack">
     <div v-if="showSwipe.right" class="swipe-bg swipe-left-area">
-      Moving to in progress
+      <p class="ma-0">Moving to</p>
+      <p class="ma-0">in progress</p>
     </div>
     <v-btn
       v-touch="{
         left: () => swipe('left'),
         right: () => swipe('right'),
       }"
-      :class="
-      `${item.cancelled && `cancelled-item`} ${isOverdue && `overdue-item`}`
-      "
+      :class="`${item.cancelled && `cancelled-item`} ${
+        isOverdue && `overdue-item`
+      }`"
       style="
         width: 100%;
         background-color: #282e35;
         color: #babdbf;
         text-transform: none;
-        height: 75px;
+        height: inherit;
         border-radius: 16px;
       "
       @click="onOrderClick()"
     >
       <v-layout>
-      <v-flex xs4 sm4 md2>
-        <v-container fill-height fluid>
-          <v-row align="center" justify="center">
-            <v-col align="center" justify="center">
-              <img :src="getImage" width="70%" />
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-flex>
-      <v-flex xs4 sm4 md6>
-        <v-container v-if="isCancelled" fill-height fluid>
-          <v-row align="center" justify="center">
-            <v-col align="left" justify="center" class="cancelled">
-              Cancelled
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-container v-else-if="isOrderFinished" fill-height fluid>
-          <v-row align="center" justify="center">
-            <v-col
-              align="left"
-              justify="center"
-              v-model="pickTimeCountDown"
-              :class="`pickup-time ${isOverdue && `overdue`}`"
-            >
-              {{ pickupTime }}
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-flex>
-      <v-flex xs4 sm4 md2>
-        <v-container fill-height fluid>
-          <v-row align="center" justify="center">
-            <v-col align="center" justify="center">
-              #{{ item.order_id }}
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-flex>
-      <v-flex xs4 sm4 md2>
-        <v-container fill-height fluid>
-          <v-row align="center" justify="center">
-            <v-col align="center" justify="center">
-              {{ item.order_lines.length }} items
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-flex>
-    </v-layout>
+        <v-flex xs4 sm4 md2>
+          <v-container fill-height fluid>
+            <v-row align="center" justify="center">
+              <v-col align="center" justify="center">
+                <img :src="getImage" width="70%" />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-flex>
+        <v-flex xs4 sm4 md6>
+          <v-container v-if="isCancelled" fill-height fluid>
+            <v-row align="center" justify="center">
+              <v-col align="left" justify="center" class="cancelled">
+                Cancelled
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-container v-else-if="isOrderFinished" fill-height fluid>
+            <v-row align="center" justify="center">
+              <v-col
+                align="left"
+                justify="center"
+                v-model="pickTimeCountDown"
+                :class="`pickup-time ${isOverdue && `overdue`}`"
+              >
+                {{ pickupTime }}
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-flex>
+        <v-flex xs4 sm4 md2>
+          <v-container fill-height fluid>
+            <v-row align="center" justify="center">
+              <v-col align="center" justify="center">
+                #{{ item.order_id }}
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-flex>
+        <v-flex xs4 sm4 md2>
+          <v-container fill-height fluid>
+            <v-row align="center" justify="center">
+              <v-col align="center" justify="center">
+                {{ item.order_lines.length }} items
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-flex>
+      </v-layout>
     </v-btn>
-    <div v-if="showSwipe.left">Moving to Finished</div>
+    <div v-if="showSwipe.left" class="swipe-bg swipe-right-area">
+      <p>Moving to</p>
+      <p>Finished</p>
+    </div>
   </div>
 </template>
 
@@ -86,7 +90,7 @@ export default {
         right: false,
       },
       pickTimeCountDown: this.$props.item.pickupTimeInMinutes,
-      pickupTime: this.$props.item.pickupTime
+      pickupTime: this.$props.item.pickupTime,
     };
   },
   computed: {
@@ -99,7 +103,6 @@ export default {
     getImage() {
       const { item } = this.$props;
       let fulfilment_source = item.fulfilment_source;
-      let imgSrc;
       switch (fulfilment_source) {
         case "Uber Eats":
           return require("~/assets/ubereats.png");
@@ -108,9 +111,8 @@ export default {
         case "Just Eat":
           return require("~/assets/justEat.png");
         default:
-          imgSrc = "";
+          return "";
       }
-      return imgSrc;
     },
     isOverdue() {
       const { item } = this.$props;
@@ -147,8 +149,8 @@ export default {
           this.pickupTime = pickupTime;
         }, 60000);
       },
-      immediate: true // This ensures the watcher is triggered upon creation
-    }
+      immediate: true, // This ensures the watcher is triggered upon creation
+    },
   },
   methods: {
     onOrderClick() {
@@ -161,7 +163,7 @@ export default {
       this.showSwipe[direction] = true;
       setTimeout(() => {
         this.showSwipe[direction] = false;
-      }, 900);
+      }, 1500);
     },
   },
 };
@@ -186,17 +188,34 @@ export default {
 }
 
 .swipe-bg {
-  background: #f6f8fa 0% 0% no-repeat padding-box;
-  padding: 5px 30px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 0px 20px;
   border-radius: 16px 0 0 16px;
   text-transform: uppercase;
 }
+.swipe-left-area,
+.swipe-right-area {
+  color: black;
+}
+.swipe-right-area {
+  text-align: left;
+}
+.swipe-left-area p {
+  width: 130%;
+}
 .swipe-left-area {
+  text-align: right;
 }
 .swipeable-stack {
-  flex-direction: row;
+  background: #f6f8fa 0% 0% no-repeat padding-box;
   display: flex;
   border-radius: 16px;
   line-height: 1;
+  overflow: hidden;
+  height: 90px;
 }
 </style>
