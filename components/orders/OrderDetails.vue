69ee23326274361bbed26c9c1efadbafe207b594
Jayanga Jayathilake <jayangaaj@gmail.com>
2021-05-04 14:02:19 +0530

    refac: updated predicted prep time value

--- components/orders/OrderDetails.vue
+++ components/orders/OrderDetails.vue
@@ -77,7 +77,7 @@
           v-if="isInProgressStatus"
           label="Predicted prep time:"
           :valueStyle="isOverDue && `overdue`"
-          :value="predictedPrepTime"
+          :value="order.pickupTime"
         />
       </div>
 
@@ -113,27 +113,6 @@ export default {
     };
   },
   computed: {
-    predictedPrepTime() {
-      const { pickupTimeInMinutes } = this.$props.order;
-
-      let h = Math.floor(pickupTimeInMinutes / 60);
-      let m = Math.floor(pickupTimeInMinutes % 60);
-
-      if (h != 0) {
-        if (h < 0) {
-          h++;
-          if (h == 0) {
-            return m + " Min";
-          } else {
-            return h + " hr " + m + " Min";
-          }
-        } else {
-          return h + " hr " + m + " Min";
-        }
-      } else {
-        return m + " Min";
-      }
-    },
     getImage() {
       const { order } = this.$props;
       switch (order.fulfilment_source) {
