1a69b1ef36ed08da02df9b05c6de4d2c2acc4df9
Nadun Dileepa <nadun.virtual@gmail.com>
2021-05-06 09:23:28 +0530

    add custom print templete new

--- components/orders/PosBill.vue
+++ components/orders/PosBill.vue
@@ -1,13 +1,13 @@
 <template>
-	<div style="border: solid 1px black; width: 350px;">
-		<table>
+	<div style="border: solid 1px black; width: 300px;">
+		<table style="width: 300px;">
 			<thead>
 				<tr>
 					<th colspan="2">Shop name</th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr style="padding: 5px;">
+				<tr style="font-size: 12px;">
 					<td>
 						Order Number: #{{item.order_id}}
 					</td>
@@ -15,39 +15,39 @@
 						Time: {{item.pos_fulfilment_time.split(" ")[1]}}
 					</td>
 				</tr>
-				<tr style="padding: 5px;">
+				<tr style="font-size: 12px;">
 					<td>
 						Date: {{item.pos_fulfilment_time.split(" ")[0]}}
 					</td>
 				</tr>
-				<tr class="pa-1">
+				<tr style="font-size: 12px;">
 					<td>
 						Order type: {{item.fulfilment_type}}
 					</td>
-					<td class="float-right">
+					<td style="float:right;">
 						Delivary Provider: {{item.fulfilment_source}}
 					</td>
 				</tr>
-				<tr class="pa-1">
+				<tr>
 					<td colspan="2">
 						<hr>
 					</td>
 				</tr>
 			</tbody>
 		</table>
-		<table>
+		<table style="width: 300px;">
 			<tbody>
-				<tr class="item-header">
-					<td class="pa-1">No.</td>
-					<td class="pa-1">Name</td>
-					<td class="pa-1">Qty</td>
-					<td style="float:right;" class="pa-1">Amount</td>
+				<tr style="font-weight: 600; font-size: 12px;" class="item-header">
+					<td>No.</td>
+					<td style="text-align: center;">Name</td>
+					<td>Qty</td>
+					<td style="text-align: center;">notes</td>
 				</tr>
-				<tr v-for="(product, index) in item.order_lines" :key="index">
-					<td class="pa-1">{{index+1}}</td>
-					<td class="pa-1">{{product.product.name}}</td>
-					<td class="pa-1">{{product.quantity}}</td>
-					<td style="float:right;" class="pa-1">£{{product.total}}</td>
+				<tr style="font-size:12px;" v-for="(product, index) in item.order_lines" :key="index">
+					<td>{{index+1}}</td>
+					<td>{{product.product.name}}</td>
+					<td>{{product.quantity}}</td>
+					<td>{{product.note}}</td>
 				</tr>
 			</tbody>
 		</table>
@@ -58,16 +58,3 @@ export default {
   props: ["item"]
 };
 </script>
-<style scoped>
-.billDiv {
-	border: solid 1px black;
-	background-color: rgb(206, 98, 98);
-}
-table tbody tr {
-	font-size: 10px;
-}
-
-.item-header {
-	font-weight: 600;
-}
-</style>
