88397343e9577526fb70fa558a10a7d07eceef38
SajithL-Aet <sajith.l@aeturnum.com>
2021-05-14 16:04:00 +0530

    fix issue of order details remaining when finished orders cleaning

--- pages/orders.vue
+++ pages/orders.vue
@@ -125,12 +125,12 @@ export default {
       if (mutation.type === "orders/setSelectedOrders") {
         this.selectedOrder = this.$store.getters["orders/getNewStateOrders"][0];
       }
-      if(mutation.type === "orders/sortFinishedOrders") {
+      if(this.currentTab == 2 && mutation.type === "orders/sortFinishedOrders") {
         this.selectedOrder = this.$store.getters["orders/getFinishedOrders"][0];
       }
     });
     this.$store.subscribeAction((action) => {
-      if (["orders/filterFinishedOrders"].includes(action.type)) {
+      if (this.currentTab == 2 && ["orders/filterFinishedOrders"].includes(action.type)) {
         this.selectedOrder = this.$store.getters["orders/getFinishedOrders"][0];
         console.log("this.selectedOrder",this.selectedOrder);
       }
